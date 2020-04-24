191e946cf3f2529bd5ec64ea36c6488093a74992
Fredowisk <paulorobertoparreira@yahoo.com.br>
2020-04-23 22:11:18 -0300

    Updating the project

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    // Busca, se existe deleta, se não existe não deleta.
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
